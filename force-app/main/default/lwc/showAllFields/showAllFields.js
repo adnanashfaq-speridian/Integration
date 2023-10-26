8d434677bcb267be009714f409a30ddd5372577f
adnanashfaq-speridian <adnan.ashfaq@speridian.com>
2023-10-26 14:34:22 +0500

    Working LWC component to select fields

--- force-app/main/default/lwc/showAllFields/showAllFields.js
+++ force-app/main/default/lwc/showAllFields/showAllFields.js
@@ -1,9 +1,12 @@
 import { LightningElement,api,track,wire } from 'lwc';
 import { getObjectInfo } from 'lightning/uiObjectInfoApi';
+import upsertMetadata from '@salesforce/apex/MetadataService.upsertMetadata';
  
 export default class ShowAllFields extends LightningElement {
     @track objectNameToGetFields = 'Account';
     @track lstFields =[];
+    @api selectedfields;
+    @api metaApiName='LWC_Lookup_Field__mdt';
     @wire(getObjectInfo, { objectApiName: '$objectNameToGetFields' })
     
     getObjectInfo({ error, data }) {
@@ -24,5 +27,41 @@ export default class ShowAllFields extends LightningElement {
     handleChange(e) {
         this.value = e.detail.value;
         console.log(this.value);
+        this.selectedfields = e.detail.value;
+    }
+
+    doSearch() {
+        upsertMetadata({ selectedFields: JSON.stringify(this.selectedfields) })
+            .then(result => {
+                console.log('result here == '+ JSON.stringify(result));
+            })
+            .catch(error => {
+                console.log('error here == '+ JSON.stringify(error));
+            });
+    }
+    
+
+    saveClick(e){
+        var fieldValues=[];
+        for(const element of this.records) {
+            var obj = {};
+            obj[element.name] = element.value;
+            fieldValues.push(obj);
+        }
+
+        updateMetadata({
+            fullName:this.metaApiName, 
+            label:this.recordName, 
+            fieldWithValues:JSON.stringify(fieldValues)
+        }).then(result =>{
+            console.log('result:'+result);
+            this.template.querySelector('c-common-toast').showToast('success', 'Deployment is in progress for metadata update');
+            return
+        }).catch(error =>{
+            console.log('Error in getSelectedCustomer Method :: ',JSON.stringify(error));
+            this.isLoading =false;
+            this.template.querySelector('c-common-toast').showToast('error', JSON.stringify(error));
+            return
+        })
     }
 }
